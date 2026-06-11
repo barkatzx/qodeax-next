473871a0b8f9bdbadd8a5bea3c248530f0aa7265
Barkatzx <barkatullah.zx@gmail.com>
2026-06-11 17:25:15 +0600

    Google ANalytics Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import WhatsappPopup from "@/components/WhatsappPopup";
+import Script from "next/script";
 import { ReactNode } from "react";
 import "./globals.css";
 
@@ -12,6 +13,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Qodeax-Engineering Ideas Into Powerful Software.</title>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2Z897PR04V"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2Z897PR04V');
+          `}
+        </Script>
       </head>
       <body>
         <Header />
